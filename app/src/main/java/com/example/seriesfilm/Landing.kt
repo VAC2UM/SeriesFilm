f7865b381fb21f2cf021c56796f1d88cc6f323c6
VAC2UM <nikita.golowanev@gmail.com>
2025-02-15 09:29:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/seriesfilm/Landing.kt
@@ -0,0 +1,11 @@
+package com.example.seriesfilm
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Landing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.landing)
+    }
+}
