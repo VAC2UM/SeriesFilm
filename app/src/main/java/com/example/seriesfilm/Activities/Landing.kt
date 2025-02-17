2cdad5ef038aed7a64073430b3d442eb71e37f65
VAC2UM <nikita.golowanev@gmail.com>
2025-02-17 23:40:00 +0300

    Added Login and Sign Up Activities

--- app/src/main/java/com/example/seriesfilm/Activities/Landing.kt
+++ app/src/main/java/com/example/seriesfilm/Activities/Landing.kt
@@ -1,7 +1,8 @@
-package com.example.seriesfilm
+package com.example.seriesfilm.Activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.seriesfilm.R
 
 class Landing : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/seriesfilm/Activities/Sign_Up.kt
@@ -0,0 +1,12 @@
+package com.example.seriesfilm.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.seriesfilm.R
+
+class Sign_Up : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
