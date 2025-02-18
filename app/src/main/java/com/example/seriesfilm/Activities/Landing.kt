f7ba78dba98e053a07b4f844506ce10bf79b418c
VAC2UM <nikita.golowanev@gmail.com>
2025-02-18 13:56:59 +0300

    Added MainActivity and added function for buttons

--- app/src/main/java/com/example/seriesfilm/Activities/Landing.kt
+++ app/src/main/java/com/example/seriesfilm/Activities/Landing.kt
@@ -1,12 +1,29 @@
 package com.example.seriesfilm.Activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.seriesfilm.R
 
 class Landing : AppCompatActivity() {
+    lateinit var login_btn: Button
+    lateinit var sign_up_btn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.landing)
+
+        login_btn = findViewById(R.id.login_btn)
+        sign_up_btn = findViewById(R.id.sign_up)
+
+        login_btn.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+        sign_up_btn.setOnClickListener {
+            val intent = Intent(this, Sign_Up::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/seriesfilm/Activities/Login.kt
+++ app/src/main/java/com/example/seriesfilm/Activities/Login.kt
@@ -1,12 +1,23 @@
 package com.example.seriesfilm.Activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.seriesfilm.R
 
 class Login : AppCompatActivity() {
+    lateinit var login_btn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_btn = findViewById(R.id.login_btn)
+
+        login_btn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
