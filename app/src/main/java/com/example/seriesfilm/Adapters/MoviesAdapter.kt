c3d02b659ebb98d9c76e6cceddd289bdf2a48dc2
VAC2UM <nikita.golowanev@gmail.com>
2025-03-14 14:02:08 +0300

    Added description for filmCard

--- app/src/main/java/com/example/seriesfilm/Adapters/MoviesAdapter.kt
+++ app/src/main/java/com/example/seriesfilm/Adapters/MoviesAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.seriesfilm.Data.SearchResult
 import com.example.seriesfilm.R
@@ -13,7 +14,11 @@ class MoviesAdapter(private var movies: List<SearchResult>) :
     RecyclerView.Adapter<MoviesAdapter.MovieViewHolder>() {
     class MovieViewHolder(item_movie: View) : RecyclerView.ViewHolder(item_movie) {
         val poster: ImageView = item_movie.findViewById(R.id.item_movie_poster)
+        val filmName: TextView = itemView.findViewById(R.id.filmName)
+        val filmYear: TextView = itemView.findViewById(R.id.filmYear)
+        val filmType: TextView = itemView.findViewById(R.id.filmType)
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
         return MovieViewHolder(view)
@@ -22,6 +27,9 @@ class MoviesAdapter(private var movies: List<SearchResult>) :
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         val movie = movies[position]
         Picasso.get().load(movie.imageUrl).into(holder.poster)
+        holder.filmName.text = movie.name
+        holder.filmYear.text = movie.year.toString()
+        holder.filmType.text = movie.type
     }
 
     override fun getItemCount(): Int = movies.size
